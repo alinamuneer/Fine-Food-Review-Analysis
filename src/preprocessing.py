7eb92da7409e24deb97ffeb4a182fb3e1af2ff05
Anna Senent <anna.senent.julian@avanade.com>
2024-04-03 14:37:04 +0200

    time control and N_SAMPLE removed in preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -13,8 +13,6 @@ sys.path.append('.')
 from settings import REVIEW_FILE_PATH, DATA_DIR
 from preprocessing_functions import clean_text, add_polarity_label
 
-N_SAMPLE = 100000
-
 
 def processing_pipeline(data_name: WindowsPath,
                         encoding: str,
@@ -25,8 +23,6 @@ def processing_pipeline(data_name: WindowsPath,
     
     # removing duplicates
     df.drop_duplicates(subset=['Text'], inplace=True)
-
-    # df = df.sample(n=N_SAMPLE, random_state=42)
 
     # add extra polarity label
     df = modify_func(df)
@@ -40,13 +36,10 @@ def processing_pipeline(data_name: WindowsPath,
 def save_processed_data(df: pd.DataFrame,
                         data_name: str) -> None:
     processed_data_path = f'{DATA_DIR}/processed'
-    df.to_csv(f'{processed_data_path}/{data_name}_processed.csv', # add _{N_SAMPLE} at end of the
-              index=False, encoding='utf-8')     # file name if you're using a sample of the data
+    df.to_csv(f'{processed_data_path}/{data_name}_processed.csv',
+              index=False, encoding='utf-8')
     
 def main(save: bool = True) -> None:
-
-    t = time()
-    print('Processing...')
 
     data_name = str(REVIEW_FILE_PATH).split('\\')[-1].split('.')[0]
     
@@ -54,9 +47,6 @@ def main(save: bool = True) -> None:
     df = processing_pipeline(REVIEW_FILE_PATH,
                                    'utf8',
                                    add_polarity_label)
-    
-    elapsed_time = round((time() - t) / 60, 2)
-    print(f'Time to clean up everything: {elapsed_time} min.')
 
     if save:
         save_processed_data(df, data_name)
