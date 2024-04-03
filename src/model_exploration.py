a0f6bf92060ba68f8078820d3182402be0fac969
Alina Munir <alina.munir@avanade.com>
2024-04-03 17:37:12 +0200

    revisited changes, defining data types in functions and automatizing the data file path

--- src/model_exploration.py
+++ src/model_exploration.py
@@ -1,17 +1,22 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from sentiment_exploration import *
-from textblob import TextBlob
+from sentiment_exploration import apply_sentiment_analysis
+import pandas as pd
+import numpy as np
+import sys
 import xgboost as xgb
 
+sys.path.append('.')
+from settings import REVIEW_FILE_PATH
 
-def apply_vectorization(df):
+def apply_vectorization(df: pd.DataFrame) -> CountVectorizer:
     # Initialize the vectorizer
     vect = CountVectorizer(ngram_range = (1,2), max_features=10000, min_df = 2, stop_words = 'english')
     return vect
 
-def xgb_classifier(vect, df):
+
+def xgb_classifier(vect : CountVectorizer, df : pd.DataFrame) -> np.ndarray:
     X_text_features = vect.fit_transform(df.combined_text_Summary)
     df[[-1]] = df[['comb_sentiment']] 
     X_sentiment = df[[-1]]
@@ -26,7 +31,7 @@ def xgb_classifier(vect, df):
 
 def main():
     # Load your DataFrame here
-    df = pd.read_csv("./data/Reviews_processed_100000.csv")
+    df = pd.read_csv(REVIEW_FILE_PATH)
 
     # Apply the sentiment analysis function to the DataFrame
     df = apply_sentiment_analysis(df)
--- src/sentiment_exploration.py
+++ src/sentiment_exploration.py
@@ -4,24 +4,10 @@ from textblob import TextBlob
 def detect_sentiment(text):
     return TextBlob(text).sentiment.polarity
 
-def apply_sentiment_analysis(df):
+def apply_sentiment_analysis(df : pd.DataFrame) -> pd.DataFrame:
     # Apply the function to each element of the column
     df.summary_cleaned.fillna('', inplace=True)
     df['combined_text_Summary'] = df['text_cleaned'] + ' ' + df['summary_cleaned']
     df['comb_sentiment'] = df.combined_text_Summary.apply(detect_sentiment)
     return df
 
-def main():
-    # Load your DataFrame here
-    df = pd.read_csv("../data/Reviews_processed_10000.csv")
-
-    # Apply the sentiment analysis function to the DataFrame
-    df = apply_sentiment_analysis(df)
-    
-    # Now df contains the cleaned columns
-    #print(df.head())
-
-
-if __name__ == "__main__":
-    main()
-    
